2ed4f8b33f86a618b3ae1f2227123ed6f0378776
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-09-06 14:08:44 +0300

    Closes #9 - Add 'HomeAdress' field to Location entity

---
+++ src/BookCrossingBackEnd/Migrations/20200905200729_Add_HomeAdress_ToLocation_Entity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookCrossingBackEnd.Migrations
+{
+    public partial class Add_HomeAdress_ToLocation_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HomeAdress",
+                table: "Location",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "State" },
+                values: new object[] { new DateTime(2020, 9, 5, 23, 7, 28, 127, DateTimeKind.Local).AddTicks(7668), "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role_id",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "role_id",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HomeAdress",
+                table: "Location");
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "State" },
+                values: new object[] { new DateTime(2020, 8, 27, 11, 58, 34, 359, DateTimeKind.Local).AddTicks(3564), "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role_id",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "role_id",
+                value: 2);
+        }
+    }
+}
