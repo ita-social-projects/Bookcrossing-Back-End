b398fadb0d4d79dd1bc504b5d8891af35d683010
Andrian Zafiiovskiy <andrian.zafiiovskiy@gmail.com>
2020-09-23 22:33:16 +0300

    Adding needed migration - God help me

---
+++ src/BookCrossingBackEnd/Migrations/20200923193003_FixingLocationHomeTables.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookCrossingBackEnd.Migrations
+{
+    public partial class FixingLocationHomeTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "LocationHome",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationHome", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "State" },
+                values: new object[] { new DateTime(2020, 9, 23, 22, 30, 2, 575, DateTimeKind.Local).AddTicks(4505), "Available" });
+
+            migrationBuilder.InsertData(
+                table: "LocationHome",
+                columns: new[] { "Id", "City", "IsActive", "Latitude", "Longitude", "Street" },
+                values: new object[] { 1, "Lviv", true, 49.826371600000002, 23.944969700000001, "Gorodoc'kogo" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role_id",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "role_id",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "role_id",
+                value: 2);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_LocationHome_home_location_id",
+                table: "User",
+                column: "home_location_id",
+                principalTable: "LocationHome",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_LocationHome_home_location_id",
+                table: "User");
+
+            migrationBuilder.DropTable(
+                name: "LocationHome");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(17)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "DateAdded", "State" },
+                values: new object[] { new DateTime(2020, 9, 21, 17, 28, 47, 41, DateTimeKind.Local).AddTicks(7498), "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role_id",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "role_id",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "role_id",
+                value: 2);
+        }
+    }
+}
