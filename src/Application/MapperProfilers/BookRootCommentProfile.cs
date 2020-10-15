c093a25178dbf3e56a7456c6e6739af69d1a59f2
Zafiiovskiy <andrian.zafiiovskiy@gmail.com>
2020-10-15 15:57:56 +0300

    Closes #203 - Fixing datetime parse (#204)

--- src/Application/MapperProfilers/BookRootCommentProfile.cs
+++ src/Application/MapperProfilers/BookRootCommentProfile.cs
@@ -9,7 +9,7 @@ namespace Application.MapperProfilers
         public BookRootCommentProfile()
         {
             CreateMap<NoSqlEntities.BookRootComment, Dto.Comment.Book.RootDto>()
-                .ForMember(dto => dto.Date, opt => opt.MapFrom(entity => DateTime.Parse(entity.Date, DateTimeFormatInfo.InvariantInfo).ToLocalTime()))
+                .ForMember(dto => dto.Date, opt => opt.MapFrom(entity => DateTime.Parse(entity.Date, null, DateTimeStyles.RoundtripKind).ToLocalTime()))
              .ForMember(dto => dto.Comments, opt => opt.MapFrom(entity => entity.Comments))
              .ForMember(dto => dto.Owner, opt => opt.MapFrom(entity => new Dto.Comment.OwnerDto() { Id = entity.OwnerId }));
         }
