60a2cbeceaeb8179897e92e2b812941f47cda7a2
Andrian Zafiiovskiy <andrian.zafiiovskiy@gmail.com>
2020-10-17 15:42:24 +0300

    Closes #221 - Fixing categories page + trying to ParseExact

--- src/Application/MapperProfilers/BookRootCommentProfile.cs
+++ src/Application/MapperProfilers/BookRootCommentProfile.cs
@@ -9,9 +9,10 @@ namespace Application.MapperProfilers
         public BookRootCommentProfile()
         {
             CreateMap<NoSqlEntities.BookRootComment, Dto.Comment.Book.RootDto>()
-                .ForMember(dto => dto.Date, opt => opt.MapFrom(entity => Convert.ToDateTime(entity.Date).ToLocalTime()))
+                .ForMember(dto => dto.Date, opt => opt.MapFrom(entity => DateTime.ParseExact(entity.Date, "o", CultureInfo.InvariantCulture, DateTimeStyles.None).ToLocalTime()))
              .ForMember(dto => dto.Comments, opt => opt.MapFrom(entity => entity.Comments))
-             .ForMember(dto => dto.Owner, opt => opt.MapFrom(entity => new Dto.Comment.OwnerDto() { Id = entity.OwnerId }));
+             .ForMember(dto => dto.Owner, opt => opt.MapFrom(entity => new Dto.Comment.OwnerDto() { Id = entity.OwnerId }))
+             .ReverseMap();
         }
     }
 }
