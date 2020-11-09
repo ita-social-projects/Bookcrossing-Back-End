ff17175cfdad8828f2f1ed6f99968b6b920606eb
Vitalii Yartym <53553415+vital9872@users.noreply.github.com>
2020-11-09 23:35:53 +0200

    Closes #337 - fix message work (#344)

---
+++ src/BookCrossingBackEnd/Migrations/20201109160055_SetMaxLength580ForNotificationMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookCrossingBackEnd.Migrations
+{
+    public partial class SetMaxLength580ForNotificationMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageUk",
+                table: "Notification",
+                maxLength: 580,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Notification",
+                maxLength: 580,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageUk",
+                table: "Notification",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 580,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Notification",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 580);
+        }
+    }
+}
